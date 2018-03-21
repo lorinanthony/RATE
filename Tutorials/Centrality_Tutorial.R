e625183a5a28ee41f6b0145b97b02f1964a23a42
Lorin Crawford <lorincrawford@Lorins-MacBook-Pro.local>
2018-03-20 22:16:46 -0400

    Updated and changed tutorials and simulations

--- Tutorials/Centrality_Tutorial.R
+++ Tutorials/Centrality_Tutorial.R
@@ -1,15 +1,20 @@
 #NOTE: This script will demonstrate orders of distributional centrality via RATE measures. Specifically it shows:
 #(1) How to compute a covariance matrix using the Gaussian kernel function;
 #(2) How to fit a standard Bayesian Gaussian process (GP) regression model;
-#(3) Retrieve effect size analogue estimates for the original predictor variables;
-#(4) Prioritize variables via their first, second, third, and fourth order centrality.
+#(3) Prioritize variables via their first, second, third, and fourth order distributional centrality.
 
 #NOTE: This script is based on a simple (and small) genetics example where we simulate 
 #genotype data for n = 500 individuals with p = 25 measured genetic variants. We randomly 
-#assume the last three predictors p∗ = {23, 24, 25} are causal and have true association with #the generated (continuous) phenotype y. We then assume that the p* predictor variables 
-#explain a fixed Vx = 75% (phenotypic variance explained; PVE) of the total variance in the 
-#response V(y). This parameter Vx can alternatively be described as a factor controlling the 
-#signal-to-noise ratio. The parameter rho represents the proportion of Vx that is contributed by additive effects versus interaction effects. Namely, the additive effects make up rho%, while the pairwise interactions make up the remaining (1 − rho)%.
+#assume the last three predictors p∗ = {23, 24, 25} are causal and have true association with 
+#the generated (continuous) phenotype y. We then assume that the p* predictive markers 
+#explain a fixed H2 % (phenotypic variance explained; PVE) of the total variance in the 
+#response V(y). This parameter H2 can alternatively be described as a factor controlling the 
+#signal-to-noise ratio. The parameter rho represents the proportion of H2 that is contributed by additive effects versus interaction effects. 
+#Namely, the additive effects make up rho%, while the pairwise interactions (epistatic effects) make up the remaining (1 − rho)%.
+
+######################################################################################
+######################################################################################
+######################################################################################
 
 ### Clear Console ###
 cat("\014")
@@ -29,10 +34,10 @@ library(RcppArmadillo)
 library(RcppParallel)
 
 ### Load in the RATE R functions ###
-source("~/RATE.R")
+source("RATE.R")
 
 ### Load in the C++ BAKR functions ###
-sourceCpp("~/BAKRGibbs.cpp")
+sourceCpp("BAKRGibbs.cpp")
 
 ######################################################################################
 ######################################################################################
@@ -41,40 +46,37 @@ sourceCpp("~/BAKRGibbs.cpp")
 ### Set the random seed to reproduce research ###
 set.seed(11151990)
 
-### Set up simulation parameters ###
-n = 500; p = 25; pve=0.75; rho=0.5;
+n = 100; p = 250; pve=0.75; rho=0.5;
+ncausal1= 3 #Set 1 of causal SNPs
 
-### Generate the data ###
+ncausal = ncausal1
+
 maf <- 0.05 + 0.45*runif(p)
 X   <- (runif(n*p) < maf) + (runif(n*p) < maf)
 X   <- matrix(as.double(X),n,p,byrow = TRUE)
-#Xmean=apply(X, 2, mean); Xsd=apply(X, 2, sd); Geno=t((t(X)-Xmean)/Xsd)
-colnames(X) = paste("SNP",1:ncol(X),sep="")
-
-### Set the number of causal variables ###
-ncausal = 3
+Xmean=apply(X, 2, mean); Xsd=apply(X, 2, sd); X=t((t(X)-Xmean)/Xsd)
 s=c(23:25)
 
-### Generate the Marginal Effects ###
+# Marginal Effects Only
 Xmarginal=X[,s]
-beta1=rep(1,ncausal)
+beta1=rep(1,ncausal) #0.5,0.5)
 y_marginal=c(Xmarginal%*%beta1)
 beta1=beta1*sqrt(pve*rho/var(y_marginal))
 y_marginal=Xmarginal%*%beta1
 
-### Generate the Epistatic Effects ###
+#Pairwise Epistatic Effects
 Xepi=cbind(X[,s[1]]*X[,s[3]],X[,s[2]]*X[,s[3]])
 beta2=c(1,1)
 y_epi=c(Xepi%*%beta2)
 beta2=beta2*sqrt(pve*(1-rho)/var(y_epi))
 y_epi=Xepi%*%beta2
 
-### Generate the Random Error Terms ###
+# error
 y_err=rnorm(n)
 y_err=y_err*sqrt((1-pve)/var(y_err))
 
-### Generate the Phenotypes ###
-y=c(y_marginal+y_epi+y_err)
+y=c(y_marginal+y_epi+y_err); #Full Model
+colnames(X) = paste("SNP",1:ncol(X),sep="")
 
 ######################################################################################
 ######################################################################################
@@ -99,19 +101,9 @@ mcmc.iter = 1e4
 fhat = Kn %*% solve(Kn + diag(n), y) #Posterior mean
 fhat.rep = rmvnorm(mcmc.iter,fhat,Kn - Kn %*% solve(Kn+ diag(n),Kn))
 
-### Get the Posterior Draws of the Effect Size Analogue ###
-beta.tilde.draws = t(ginv(X) %*% t(fhat.rep)) #Posterior Draws
-
 #NOTE: We formally define the effect size analogue as the result of projecting the design 
 #matrix X onto the nonlinear response vector f, where beta = Proj(X,f) = X^+f with X^+ 
 #symbolizing the Moore-Penrose generalized inverse.
-
-### Find the Approximate Posterior Mean, Covariance, and Precision ###
-mu = colMeans(beta.tilde.draws)
-Sigma = cov(beta.tilde.draws); Sigma = as.matrix(nearPD(Sigma)$mat)
-Lambda = ginv(Sigma)
-
-### NOTE: Sometimes it is worth using the shrinkage version of the covariance matrix V = cov.shrink(V). This can help stabilize the KLD and RATE computations ### 
 
 ######################################################################################
 ######################################################################################
@@ -122,7 +114,7 @@ cores = cores=detectCores()
 
 ### Run the RATE Function ###
 nl = NULL
-res = RATE(mu,Sigma,Lambda,snp.nms = colnames(X),cores = cores)
+res = RATE(X=X,f.draws=fhat.rep,snp.nms = colnames(X),cores = cores)
 
 #The function results in a list with: 
 #(1) The raw Kullback-Leibler divergence measures (RATE$KLD); 
@@ -136,7 +128,7 @@ DELTA = res$Delta
 ESS = res$ESS
 
 ### Plot the results with the uniformity line ###
-barplot(rates,xlab = "Covariates",ylab=expression(RATE(tilde(beta)[j])),names.arg ="",col = ifelse(c(1:length(mu))%in%s,"blue","grey80"),border=NA,cex.names = 0.6,ylim=c(0,0.6),cex.lab=1.25,cex.axis = 1.25)
+barplot(rates,xlab = "Covariates",ylab=expression(RATE(tilde(beta)[j])),names.arg ="",col = ifelse(c(1:p)%in%s,"blue","grey80"),border=NA,cex.names = 0.6,ylim=c(0,0.6),cex.lab=1.25,cex.axis = 1.25)
 lines(x = -0.5:30.5,y = rep(1/(p-length(nl)),32),col = "red",lty=2,lwd=2)
 legend("topleft",legend=c(as.expression(bquote(DELTA~"="~.(round(DELTA,3)))),as.expression(bquote("ESS ="~.(round(ESS,2))*"%"))),bty = "n",pch = 20,col = "red")
 
@@ -144,11 +136,10 @@ legend("topleft",legend=c(as.expression(bquote(DELTA~"="~.(round(DELTA,3)))),as.
 ######################################################################################
 ######################################################################################
 
-### Find Second Order Centrality by Nullifying the most Associated Predictor Variable ###
-
 ### Run the RATE Function ###
-nl = which(res$KLD%in%sort(res$KLD,decreasing=TRUE)[1])
-res2 = RATE(mu,Sigma,Lambda,nullify = nl,snp.nms = colnames(X),cores = cores)
+top = substring(names(res$KLD)[order(res$KLD,decreasing=TRUE)[1]],first = 4)
+nl = c(nl,as.numeric(top))  
+res2 = RATE(X=X,f.draws=fhat.rep,nullify = nl,snp.nms = colnames(X),cores = cores)
 
 ### Get the Results ###
 rates = res2$RATE
@@ -156,7 +147,7 @@ DELTA = res2$Delta
 ESS = res2$ESS
 
 ### Plot the results with the uniformity line ###
-barplot(rates,xlab = "Covariates",ylab=bquote(RATE(tilde(beta)[j]~"|"~tilde(beta)[.(as.integer(nl))]=="0")),names.arg = "",col = ifelse(c(1:length(mu))[-nl]%in%s,"blue","grey80"),border=NA,cex.names = 0.6,ylim=c(0,0.6),cex.lab=1.25,cex.axis = 1.25)
+barplot(rates,xlab = "Covariates",ylab=bquote(RATE(tilde(beta)[j]~"|"~tilde(beta)[.(as.integer(nl))]=="0")),names.arg = "",col = ifelse(c(1:p)[-nl]%in%s,"blue","grey80"),border=NA,cex.names = 0.6,ylim=c(0,0.6),cex.lab=1.25,cex.axis = 1.25)
 lines(x = -0.5:29.5,y = rep(1/(p-length(nl)),31),col = "red",lty=2,lwd=2)
 legend("topleft",legend=c(as.expression(bquote(DELTA~"="~.(round(DELTA,3)))),as.expression(bquote("ESS ="~.(round(ESS,2))*"%"))),bty = "n",pch = 20,col = "red")
 
@@ -167,8 +158,9 @@ legend("topleft",legend=c(as.expression(bquote(DELTA~"="~.(round(DELTA,3)))),as.
 ### Find Third Order Centrality by Nullifying the most Associated Predictor Variable ###
 
 ### Run the RATE Function ###
-nl = c(nl,which(res2$KLD%in%sort(res2$KLD,decreasing=TRUE)[1]))
-res3 = RATE(mu,Sigma,Lambda,nullify = nl,snp.nms = colnames(X),cores = cores)
+top = substring(names(res2$KLD)[order(res2$KLD,decreasing=TRUE)[1]],first = 4)
+nl = c(nl,as.numeric(top))
+res3 = RATE(X=X,f.draws=fhat.rep,nullify = nl,snp.nms = colnames(X),cores = cores)
 
 ### Get the Results ###
 rates = res3$RATE
@@ -176,7 +168,7 @@ DELTA = res3$Delta
 ESS = res3$ESS
 
 ### Plot the results with the uniformity line ###
-barplot(rates,xlab = "Covariates",ylab=bquote(RATE(tilde(beta)[j]~"|"~tilde(beta)[.(as.integer(nl))]=="0")),names.arg = "",col = ifelse(c(1:length(mu))[-nl]%in%s,"blue","grey80"),border=NA,cex.names = 0.6,ylim=c(0,0.6),cex.lab=1.25,cex.axis = 1.25)
+barplot(rates,xlab = "Covariates",ylab=bquote(RATE(tilde(beta)[j]~"|"~tilde(beta)[.(as.integer(nl))]=="0")),names.arg = "",col = ifelse(c(1:p)[-nl]%in%s,"blue","grey80"),border=NA,cex.names = 0.6,ylim=c(0,0.6),cex.lab=1.25,cex.axis = 1.25)
 lines(x = -0.5:28.5,y = rep(1/(p-length(nl)),30),col = "red",lty=2,lwd=2)
 legend("topleft",legend=c(as.expression(bquote(DELTA~"="~.(round(DELTA,3)))),as.expression(bquote("ESS ="~.(round(ESS,2))*"%"))),bty = "n",pch = 20,col = "red")
 
@@ -187,8 +179,9 @@ legend("topleft",legend=c(as.expression(bquote(DELTA~"="~.(round(DELTA,3)))),as.
 ### Find Fourth Order Centrality by Nullifying the most Associated Predictor Variable ###
 
 ### Run the RATE Function ###
-nl = c(nl,which(res3$KLD%in%sort(res3$KLD,decreasing=TRUE)[1]))
-res4 = RATE(mu,Sigma,Lambda,nullify = nl,snp.nms = colnames(X),cores = cores)
+top = substring(names(res3$KLD)[order(res3$KLD,decreasing=TRUE)[1]],first = 4)
+nl = c(nl,as.numeric(top))
+res4 = RATE(X=X,f.draws=fhat.rep,nullify = nl,snp.nms = colnames(X),cores = cores)
 
 ### Get the Results ###
 rates = res4$RATE
@@ -196,6 +189,6 @@ DELTA = res4$Delta
 ESS = res4$ESS
 
 ### Plot the results with the uniformity line ###
-barplot(rates,xlab = "Covariates",ylab=bquote(RATE(tilde(beta)[j]~"|"~tilde(beta)[.(as.integer(nl))]=="0")),names.arg = "",col = ifelse(c(1:length(mu))[-nl]%in%s,"blue","grey80"),border=NA,cex.names = 0.6,ylim=c(0,0.6),cex.lab=1.25,cex.axis = 1.25)
+barplot(rates,xlab = "Covariates",ylab=bquote(RATE(tilde(beta)[j]~"|"~tilde(beta)[.(as.integer(nl))]=="0")),names.arg = "",col = ifelse(c(1:p)[-nl]%in%s,"blue","grey80"),border=NA,cex.names = 0.6,ylim=c(0,0.6),cex.lab=1.25,cex.axis = 1.25)
 lines(x = -0.5:27.5,y = rep(1/(p-length(nl)),29),col = "red",lty=2,lwd=2)
 legend("topleft",legend=c(as.expression(bquote(DELTA~"="~.(round(DELTA,3)))),as.expression(bquote("ESS ="~.(round(ESS,2))*"%"))),bty = "n",pch = 20,col = "red")
---
+++ Tutorials/Power_Comparisons.R
@@ -0,0 +1,253 @@
+#NOTE: This script will demonstrate the power of distributional centrality via RATE measures. Specifically it compares:
+#(1) L1- regularized lasso regression; 
+#(2) The combined regularization utilized by the elastic net;
+#(3) A commonly used spike and slab prior model, also known as Bayesian variable selection regression, which computes posterior inclusion probabilities (PIPs) 
+#for each covariate as a mixture of a point mass at zero and a diffuse normal centered around zero.
+#(4) A genome scan with individual single nucleotide polymorphisms (SNPs) fit via a univariate linear model (SCANONE).
+
+#NOTE: This script is based on the genetics simulations from Crawford et al. (2018). Here, we simulate 
+#genotype data for n = 500 individuals with p = 2500 independent genetic variants. We randomly 
+#assume that j∗ = 30 are causal and have true association with the generated (continuous)
+#phenotype y. We then assume that the j* predictor variables explain a fixed H2%
+#(phenotypic variance explained; PVE) of the total variance in the response V(y). This
+#parameter H2 can alternatively be described as a factor controlling the signal-to-noise ratio.The 
+#parameter rho represents the proportion of H2 that is contributed by additive effects versus
+#interaction (epistatic) effects. Namely, the additive effects make up rho%, while the pairwise interactions 
+#make up the remaining (1 − rho)%.
+
+#NOTE: We also include a scenario where population stratification effects are introduced into the simulations 
+#by allowing the top 5 genotype principal components (PCs) Z to make up an additional% of the overall variation 
+#in the trait (see variable "pc.var"). The effect sizes for these stratification effects are also drawn 
+#from a standard normal distribution. Alternatively, one can think of the combined effect of the PCs as structured noise.
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+### Clear Console ###
+cat("\014")
+
+### Clear Environment ###
+rm(list = ls(all = TRUE))
+
+### Load in the R libraries ###
+library(adegenet)
+library(corpcor)
+library(doParallel)
+library(glmnet)
+library(MASS)
+library(Matrix)
+library(mvtnorm)
+library(Rcpp)
+library(RcppArmadillo)
+library(RcppParallel)
+library(varbvs)
+
+### Load in the RATE R functions ###
+source("Software/RATE.R")
+
+### Load in the C++ BAKR functions ###
+sourceCpp("BAKRGibbs.cpp")
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+### Define the Compute Power Function ###
+compute.power <- function(pvals,SNPs){
+  nsnps = length(pvals)
+  Pos = SNPs #True Positive Set
+  Negs = names(pvals)[which(names(pvals)%in%SNPs==FALSE)] #True Negative Set
+  x = foreach(i = 1:nsnps)%dopar%{
+    v = sort(pvals,decreasing = TRUE)[1:i] #Test Positives
+    z = pvals[which(names(pvals)%in%names(v)==FALSE)] #Test Negatives
+    
+    TP = length(which(names(v)%in%Pos==TRUE))
+    FP = length(which(names(v)%in%Pos==FALSE))
+    TN = length(which(names(z)%in%Negs==TRUE))
+    FN = length(which(names(z)%in%Negs==FALSE))
+    
+    TPR = TP/(TP+FN); FPR = FP/(FP+TN); FDR = FP/(FP+TP)
+    c(TPR,FPR,FDR)
+  }
+  return(matrix(unlist(x),ncol = 3,byrow = TRUE))
+}
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+### Set the random seed to reproduce research ###
+set.seed(11151990)
+
+### Specify the Number of Samples and Genetic Markers ###
+n = 500; p = 2500; 
+
+### Set up simulation parameters ###
+pve=0.6; rho=0.5; pc.var = 0; ncausal = 30
+
+### The Number of Causal Variables ###
+ncausal1= 10 #Set 1 of causal SNPs 
+ncausal2 = 10 #Set 2 of Causal SNPs
+ncausal3 = ncausal-(ncausal1+ncausal2) #Set 3 of Causal SNPs with only marginal effects
+
+### Generate the data ###
+maf <- 0.05 + 0.45*runif(nsnp)
+X   <- (runif(ind*nsnp) < maf) + (runif(ind*nsnp) < maf)
+X   <- matrix(as.double(X),ind,nsnp,byrow = TRUE); Geno = X
+Xmean=apply(X, 2, mean); Xsd=apply(X, 2, sd); X=t((t(X)-Xmean)/Xsd)
+s=sample(1:nsnp,ncausal,replace = FALSE)
+
+#Select Causal SNPs
+s1=sample(s, ncausal1, replace=F)
+s2=sample(s[s%in%s1==FALSE], ncausal2, replace=F)
+s3=sample(s[s%in%c(s1,s2)==FALSE], ncausal3, replace=F)
+
+#Generate the ground-truth regression coefficients for the variables (X). 
+#Adjust the effects so that the variables (SNPs) explain x percent of the variance in the outcome.
+
+Xcausal1=X[,s1]; Xcausal2=X[,s2];
+Xepi=c()
+for(i in 1:ncausal1){
+  Xepi=cbind(Xepi,Xcausal1[,i]*Xcausal2)
+}
+dim(Xepi)
+
+# Marginal Effects Only
+Xmarginal=cbind(X[,s],X[,-s])
+beta=c(rnorm(dim(X[,s])[2]),rnorm(nsnp-length(s),sd = 1e-3^2))
+y_marginal=c(Xmarginal%*%beta)
+beta=beta*sqrt(pve*rho/var(y_marginal))
+y_marginal=Xmarginal%*%beta
+
+#Pairwise Epistatic Effects
+beta=rnorm(dim(Xepi)[2])
+y_epi=c(Xepi%*%beta)
+beta=beta*sqrt(pve*(1-rho)/var(y_epi))
+y_epi=Xepi%*%beta
+
+### Compute the Top PCs ###
+PCs = ComputePCs(X,5)
+
+### Define the effects of the PCs ###
+beta=rnorm(dim(PCs)[2])
+y_pcs=c(PCs%*%beta)
+beta=beta*sqrt(pc.var/var(y_pcs))
+y_pcs=PCs%*%beta
+
+### Error Term ###
+y_err=rnorm(ind)
+y_err=y_err*sqrt((1-pve-pc.var)/var(y_err))
+
+### Simulate the Response ###
+y=c(y_marginal+y_epi+y_pcs+y_err) #Full Model
+y=(y-mean(y))/(sd(y)) #Centered and Scaled
+
+s = c(s1,s2,s3)
+colnames(X) = paste("SNP",1:ncol(X),sep="")
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+### Running the Bayesian Gaussian Process (GP) Regression Model ###
+
+### Create the Nonlinear Covariance Matrix with the Gaussian Kernel ###
+#This function takes on two arguments:
+#(1) The Genotype matrix X. This matrix should be fed in as a pxn matrix. That is, predictor
+#are the rows and subjects/patients/cell lines are the columns.
+#(2) The bandwidth (also known as a smoothing parameter or lengthscale) h. For example, the 
+#Gaussian kernel can be specified as k(u,v) = exp{||u−v||^2/2h^2}.
+
+h = median(as.matrix(dist(X)))
+Kn = GaussKernel(t(X),1/(h^2*2)); diag(Kn)=1 # 
+
+### Set up the desired number of posterior draws ###
+mcmc.iter = 1e4
+
+### Fit the GP Regression ###
+fhat = Kn %*% solve(Kn + diag(n), y) #Posterior mean
+fhat.rep = rmvnorm(mcmc.iter,fhat,Kn - Kn %*% solve(Kn+ diag(n),Kn))
+
+#NOTE: We formally define the effect size analogue as the result of projecting the design 
+#matrix X onto the nonlinear response vector f, where beta = Proj(X,f) = X^+f with X^+ 
+#symbolizing the Moore-Penrose generalized inverse.
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+### Compute the First Order Centrality of each Predictor Variable ###
+cores = cores=detectCores()
+
+### Run the RATE Function ###
+nl = NULL
+res = RATE(X=X,f.draws=fhat.rep,snp.nms = colnames(X),cores = cores)
+
+#The function results in a list with: 
+#(1) The raw Kullback-Leibler divergence measures (RATE$KLD); 
+#(2) The relative centrality measures (RATE$RATE); 
+#(3) The entropic deviance from uniformity (RATE$Detla); and 
+#(4) The calibrating approximate effect sample size (ESS) measures from importance sampling (Gruber and West, 2016, 2017)
+
+### Get the Results ###
+rates = res$RATE
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+### Bayes Variable Selection ###
+fit = varbvs(X,Z=NULL,y)
+pips = fit$pip
+
+### LASSO ###
+fit= cv.glmnet(X, y,intercept=FALSE,alpha=1)
+lasso = as.matrix(coef(fit,s = fit$lambda.1se))
+lasso = c(lasso[-1,])
+
+### Elastic Net ###
+fit= cv.glmnet(X, y,intercept=FALSE,alpha=0.5)
+enet = as.matrix(coef(fit,s = fit$lambda.1se))
+enet = c(enet[-1,])
+
+### Scan One ###
+lp_scanone = sapply(1:ncol(X),function(i) -log10(summary(lm(y~X[,i]))$coef[2,4]))
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+### Compute the Power of Each Metric ###
+b = rates; names(b) = colnames(X)
+power.rate = compute.power(b,colnames(X)[s])
+
+b = pips
+power.pips= compute.power(b,colnames(X)[s])
+
+b = abs(lasso); names(b) = colnames(X)
+power.lasso= compute.power(b,colnames(X)[s])
+
+b = abs(enet); names(b) = colnames(X)
+power.enet= compute.power(b,colnames(X)[s])
+
+b = lp_scanone; names(b) = colnames(X)
+power.scanone = compute.power(b,colnames(X)[s])
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+### Save the Results ###
+PP = cbind(power.rate,power.pips,power.lasso,power.enet,power.scanone)
+auc = c(trapz(PP[,2],PP[,1]),trapz(PP[,5],PP[,4]),trapz(PP[,8],PP[,7]),trapz(PP[,11],PP[,10]),trapz(PP[,14],PP[,13]))
+m = paste(c("RATE","PIPs","LASSO","Elastic Net","SCANONE")," (", round(auc,3), ") ",sep = "")
+
+### Visualize these power comparisons ###
+plot(PP[,2],PP[,1],type = "l",lty = 1, lwd = 2, col = "blue", xlab = "False Positive Rate",ylab = "True Positive Rate", ylim = c(0,1),xlim = c(0,1),bty = "n")
+lines(PP[,5],PP[,4],type = "l",lty = 4, lwd = 2, col = "magenta")
+lines(PP[,8],PP[,7],type = "l",lty = 5, lwd = 2, col = "dark red")
+lines(PP[,11],PP[,10],type = "l",lty = 6, lwd = 2, col = "forest green")
+lines(PP[,14],PP[,13],type = "l",lty = 2, lwd = 2, col = "orange3")
+legend("bottomright",legend = m, lty =c(1,4,5,6,2), lwd = c(2,2,2,2,2),col = c("blue","magenta","dark red","forest green","orange3"),ncol=2,bty = "n",title = expression(bold("Method (AUC):")))
+abline(a=0, b=1,col="black")

9cf8e101be41e39bb3f53f06321eb93acad312e6
Lorin Crawford <lorin_crawford@brown.edu>
2018-08-26 22:10:21 -0400

    Updated the Software

--- Software/RATE.R
+++ Software/RATE.R
@@ -1,49 +1,56 @@
-### Predictor Prioritization via RelATive cEntrality (RATE) centrality measures ###
+### Variable Prioritization via RelATive cEntrality (RATE) centrality measures ###
 
 ### NOTE: This function assumes that one has already obtained (posterior) draws/estimates of a nonparametric or nonlinear function as suggested in Crawford et al. (2018) ###
 
 ### Review of the function parameters ###
 #'X' is the nxp design matrix (e.g. genotypes) where n is the number of samples and p is the number of dimensions. This is the original input data
 #'f.draws' is the Bxn matrix of the nonparametric model estimates (i.e. f.hat) with B being the number of sampled (posterior) draws;
-#'nullify' is an optional vector specifying a given predictor variable effect that user wishes to "remove" from the data set. An example of this corresponds to Figures 2(b)-(d) in Crawford et al. (2018);
+#'prop.var' is the desired proportion of variance that the user wants to explain when applying singular value decomposition (SVD) to the design matrix X (this is preset to 1);
+#'low.rank' is a boolean variable detailing if the function will use low rank matrix approximations to compute the RATE values --- note that this highly recommended in the case that the number of covariates (e.g. SNPs, genetic markers) is large; 
+#'rank.r' is a parameter detailing which matrix rank order to compute the RATE measures --- the default is min(n,p); however, this can be specified to a smaller value at the possible cost of accuracy and power;
+#'nullify' is an optional vector specifying a given predictor variable effect that user wishes to "remove" from the data set;
 #'snp.nms' is an optional vector specifying the names of the predictor variables;
 #'cores' is a parameter detailing the number of cores to parallelize over. If too many are assigned, RATE will set this variable to maximum number of cores that are available on the operating machine.
 
-#NOTE: As decribed in the Supplementary Material of Crawford et al. (2018), we implement different matrix factorizations
-#for two cases: (i) n > p, and (ii) n < p. Again, n is the number of samples and p is the number of predictors.
-
 ######################################################################################
 ######################################################################################
 ######################################################################################
 
-RATE = function(X = X, f.draws = f.draws, nullify = NULL, snp.nms = snp.nms, cores = 1){
+RATE = function(X = X, f.draws = f.draws,prop.var = 1, low.rank = FALSE, rank.r = min(nrow(X),ncol(X)), nullify = NULL,snp.nms = snp.nms, cores = 1){
   
   ### Install the necessary libraries ###
   usePackage("doParallel")
   usePackage("MASS")
   usePackage("Matrix")
+  usePackage("svd")
   
   ### Determine the number of Cores for Parallelization ###
   if(cores > 1){
     if(cores>detectCores()){warning("The number of cores you're setting is larger than detected cores!");cores = detectCores()}
   }
   
+  ### Register those Cores ###
+  registerDoParallel(cores=cores)
+  
   ### First Run the Matrix Factorizations ###  
-  if(nrow(X) < ncol(X)){
-    #In case X has linearly dependent columns, first take SVD of X to get v.
-    svd_X = svd(X)
-    r_X = sum(svd_X$d>1e-10)
-    u = with(svd_X,(1/d[1:r_X]*t(u[,1:r_X])))
-    v = svd_X$v[,1:r_X]
-    
-    # Now, calculate Sigma_star
-    SigmaFhat = cov(f.draws)
-    Sigma_star = u %*% SigmaFhat %*% t(u)
-    
-    # Now, calculate U st Lambda = U %*% t(U)
-    svd_Sigma_star = svd(Sigma_star)
-    r = sum(svd_Sigma_star$d > 1e-10)
-    U = t(ginv(v)) %*% with(svd_Sigma_star, t(1/sqrt(d[1:r])*t(u[,1:r])))
+  svd_X = propack.svd(X,rank.r); 
+  dx = svd_X$d > 1e-10
+  px = cumsum(svd_X$d^2/sum(svd_X$d^2)) < prop.var
+  r_X = dx&px 
+  u = with(svd_X,(1/d[r_X]*t(u[,r_X])))
+  v = svd_X$v[,r_X]  
+  
+  if(low.rank==TRUE){
+  # Now, calculate Sigma_star
+  SigmaFhat = cov(f.draws)
+  Sigma_star = u %*% SigmaFhat %*% t(u)
+  
+  # Now, calculate U st Lambda = U %*% t(U)
+  svd_Sigma_star = propack.svd(Sigma_star,rank.r)
+  r = svd_Sigma_star$d > 1e-10
+  U = t(ginv(v)) %*% with(svd_Sigma_star, t(1/sqrt(d[r])*t(u[,r])))
+  
+  mu = v%*%u%*%colMeans(f.draws)
   }else{
     beta.draws = t(ginv(X)%*%t(f.draws))
     V = cov(beta.draws); #V = as.matrix(nearPD(V)$mat)
@@ -51,26 +58,29 @@ RATE = function(X = X, f.draws = f.draws, nullify = NULL, snp.nms = snp.nms, cor
     svd_D = svd(D)
     r = sum(svd_D$d>1e-10)
     U = with(svd_D,t(sqrt(d[1:r])*t(u[,1:r])))
+    
+    mu = colMeans(beta.draws)
   }
+  
+  ### Create Lambda ###
   Lambda = tcrossprod(U)
   
   ### Compute the Kullback-Leibler divergence (KLD) for Each Predictor ###
-  mu = c(ginv(X)%*%colMeans(f.draws))
   int = 1:length(mu); l = nullify;
   
   if(length(l)>0){int = int[-l]}
   
   if(nrow(X) < ncol(X)){
-    KLD = foreach(j = int)%dopar%{
+    KLD = foreach(j = int, .combine='c')%dopar%{
       q = unique(c(j,l))
-      m = mu[q]
+      m = abs(mu[q])
       U_Lambda_sub = qr.solve(U[-q,],Lambda[-q,q,drop=FALSE])
       kld = crossprod(U_Lambda_sub%*%m)/2
       names(kld) = snp.nms[j]
       kld
     }
   }else{
-    KLD = foreach(j = int)%dopar%{
+    KLD = foreach(j = int, .combine='c')%dopar%{
       q = unique(c(j,l))
       m = mu[q]
       alpha = t(Lambda[-q,q])%*%ginv(as.matrix(nearPD(Lambda[-q,-q])$mat))%*%Lambda[-q,q]
@@ -79,7 +89,6 @@ RATE = function(X = X, f.draws = f.draws, nullify = NULL, snp.nms = snp.nms, cor
       kld
     }
   }
-  KLD = unlist(KLD)
   
   ### Compute the corresponding “RelATive cEntrality” (RATE) measure ###
   RATE = KLD/sum(KLD)
